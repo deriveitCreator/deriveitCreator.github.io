822191c0d706001d7944c607548553a56a685396
Uzair <uzair.99@hotmail.com>
2023-03-18 01:53:39 -0400

    body1 ad

--- desktop.js
+++ desktop.js
@@ -166,7 +166,7 @@ var helpEl;
 var headerbColor;
 var checkId = document.getElementsByTagName("BODY")[0].id;
 if (checkId == "body1"){
-	$("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="icon" type="image/png" href="../pictures/logo.png">').ready(function(){
+	$("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="icon" type="image/png" href="../pictures/logo.png"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4860967711062471" crossorigin="anonymous"></script>').ready(function(){
 		$("p").addClass("plink");
 		$(".plink a").css({"color":$("header").css("background-color")});
 	});
